6b453e409c2b5d62f27e9a70767e59415fd96304
ykchoi <ykchoi815@ahranta.com>
2024-03-27 10:12:52 +0900

    라이브러리 최신화 그래이들 최신화

--- app/src/main/java/com/codingwithtashi/paging3app/util/Utils.java
+++ app/src/main/java/com/codingwithtashi/paging3app/util/Utils.java
@@ -4,6 +4,6 @@ package com.codingwithtashi.paging3app.util;
  * Created by kunchok on 09/03/2021
  */
 public class Utils {
-    public static String API_KEY = "YOUR_API_KEY";
+    public static String API_KEY = "d9d5274a89eec20ca7d632db638405e0";
     public static String BASE_URL = "https://api.themoviedb.org/3/";
 }
